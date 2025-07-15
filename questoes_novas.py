7fe59c9d636c4655273e06cada8df585b14eb393
José Eduardo de Abreu Capito <du3xtre@gmail.com>
2025-07-15 08:48:04 -0300

    questoes_novas

---
+++ questoes_novas.py
@@ -0,0 +1,14 @@
+dic_q1 = {'titulo': 'Quem é a identidade secreta do Batman?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Bruce Wayne', 'B': 'Tony Stark', 'C': 'Peter Parker', 'D': 'Barry Allen'},
+          'correta': 'A'}
+
+dic_q2 = {'titulo': 'Qual a média de vida de um brasileiro em anos?',
+          'nivel': 'medio',
+          'opcoes': {'A': '19', 'B': '85', 'C': '76', 'D': '99'},
+          'correta': 'C'}
+
+dic_q3 = {'titulo': 'Quantas lágrimas são derramadas por dia para os dois olhos?',
+          'nivel': 'dificil',
+          'opcoes': {'A': '3.4 mm', 'B': '4.5 mm', 'C': '1. 5 mm', 'D': '9 mm'},
+          'correta': 'A'}
